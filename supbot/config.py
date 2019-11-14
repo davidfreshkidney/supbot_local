b6943e20cf3b488402a6622c6786010d29e19387
Leo Chen <yanlinchen@outlook.com>
2019-11-14 01:00:30 -0600

    null

--- supbot/config.py
+++ supbot/config.py
@@ -11,7 +11,7 @@ keys = {
 	"address": "1000 5th Ave",
 	"zip": "10028",
 	"city": "New York",
-	"card_num": "5122301555078553",
+	"card_num": "1234567890111",
 	"cvv": "531",
 	"card_month": "5",
 	"card_year": "2023"
